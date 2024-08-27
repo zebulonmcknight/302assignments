f7b323c23d356259be71d2e8daf4050b1cb64a35
zebulonmcknight <zmcknigh@vols.utk.edu>
2024-08-27 11:53:11 -0400

    final

--- challenge02/solution.cpp
+++ challenge02/solution.cpp
@@ -1,5 +1,5 @@
 // Challenge 02: Closest Numbers
-// Name:  xxxxx
+// Name:  Zebulon Mcknight
 
 // Brief description:
 
@@ -15,6 +15,66 @@ using namespace std;
 
 // Main Execution
 
+vector <int> smallestDifference(vector <int> &arr)
+{
+	vector <int> answer; 
+	int minDiff = INT_MAX;
+
+	for (size_t i = 0; i < arr.size() - 1; i++)
+	{ 
+		int diff = arr[i+1] - arr[i]; 
+		
+		if (diff < minDiff)
+		{
+			answer.clear(); 
+			minDiff = diff; 
+			answer.push_back(arr[i]); 
+			answer.push_back(arr[i+1]); 
+		} 
+
+		else if (diff == minDiff)
+		{
+			answer.push_back(arr[i]); 
+			answer.push_back(arr[i+1]); 
+		} 
+	}
+
+	return answer; 
+} 
+
 int main(int argc, char *argv[]) {
+	
+	int size;
+	vector <int> arr; 
+
+	while (cin>>size)
+	{
+		for (int i = 0; i < size; i++)
+		{
+			int val; 
+			cin>>val; 
+			arr.push_back(val);  
+		} 
+
+		sort(arr.begin(), arr.end()); 
+
+		vector <int> answer = smallestDifference(arr); 
+
+		for (size_t i = 0; i < answer.size(); i++)
+		{
+			if (i != 0)
+			{
+				cout<<" "<<answer[i]; 
+			} 
+			else 
+			{
+				cout<<answer[i]; 
+			} 
+		} 
+		cout<<endl; 
+
+		arr.clear(); 
+	} 
+
     return EXIT_SUCCESS;
 }
