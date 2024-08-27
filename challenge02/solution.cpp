c953d43786c155d9c36bf3f4e07ae433164bdbfa
zebulonmcknight <zmcknigh@vols.utk.edu>
2024-08-27 12:01:03 -0400

    final final

--- challenge02/solution.cpp
+++ challenge02/solution.cpp
@@ -1,9 +1,8 @@
 // Challenge 02: Closest Numbers
 // Name:  Zebulon Mcknight
 
-// Brief description:
-
-// This code solves yyyy based on the following idea/implementation...
+// Brief description: This program accepts an array of integers and returns 
+// the pair of items within the array with the smallest difference. 
 
 #include <algorithm>
 #include <climits>
@@ -13,15 +12,17 @@
 
 using namespace std;
 
-// Main Execution
-
+// smallestDifference accepts a sorted array by reference and returns a vector
+// containing the pair or pairs of items with the smallest difference. 
 vector <int> smallestDifference(vector <int> &arr)
 {
 	vector <int> answer; 
 	int minDiff = INT_MAX;
 
+	//For every item in the array
 	for (size_t i = 0; i < arr.size() - 1; i++)
 	{ 
+		//Calculate difference between each pair. 
 		int diff = arr[i+1] - arr[i]; 
 		
 		if (diff < minDiff)
@@ -31,7 +32,8 @@ vector <int> smallestDifference(vector <int> &arr)
 			answer.push_back(arr[i]); 
 			answer.push_back(arr[i+1]); 
 		} 
-
+		
+		//If a pair ties the minimum difference, return both pairs. 
 		else if (diff == minDiff)
 		{
 			answer.push_back(arr[i]); 
@@ -46,9 +48,11 @@ int main(int argc, char *argv[]) {
 	
 	int size;
 	vector <int> arr; 
-
+	
+	//While their are more arrays
 	while (cin>>size)
 	{
+		//Read each array into a vector 
 		for (int i = 0; i < size; i++)
 		{
 			int val; 
@@ -57,9 +61,11 @@ int main(int argc, char *argv[]) {
 		} 
 
 		sort(arr.begin(), arr.end()); 
-
+		
+		//Getting array of pairs with smallest differences
 		vector <int> answer = smallestDifference(arr); 
 
+		//Printing
 		for (size_t i = 0; i < answer.size(); i++)
 		{
 			if (i != 0)
@@ -73,6 +79,7 @@ int main(int argc, char *argv[]) {
 		} 
 		cout<<endl; 
 
+		//Clearing array for next loop
 		arr.clear(); 
 	} 
 
