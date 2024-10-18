91625f2938d92abb86c2e011f447549275fc3dc5
zebulonmcknight <zmcknigh@vols.utk.edu>
2024-10-18 15:28:45 -0400

    final

--- challenge06/solution.cpp
+++ challenge06/solution.cpp
@@ -1,61 +1,75 @@
+// Zebulon Mcknight
+// CS302 Challenge 6 - MST using Prims Algorithm 
+// 10/18/2024
+// This program uses prims algorithm to construct the minimum spanning tree of 
+// a weighted, undirected graph. 
+//
+// I referenced both chatGPT and a Geeks for Geeks article to help develop this 
+// program. https://www.geeksforgeeks.org/prims-minimum-spanning-tree-mst-greedy-algo-5/
+
 #include <iostream>
 #include <vector>
-#include <climits>  // For INT_MAX
-#include <algorithm>  // For std::sort
+#include <climits>
+#include <algorithm>  
 using namespace std;
 
-// Function to find the vertex with the minimum key value, from the set of vertices not yet included in MST
+// minKey loops through all vertices not yet in the MST and finds the vertex
+// with the minimum edgeweight to be connected to the MST. 
 int minKey(vector<int> &key, vector<bool> &inMST, int nvertices) {
     int min = INT_MAX;
     int minIndex;
 
-    for (int v = 0; v < nvertices; v++)
-        if (!inMST[v] && key[v] < min) {
-            min = key[v];
-            minIndex = v;
+    for (int i = 0; i < nvertices; i++)
+        if (!inMST[i] && key[i] < min) {
+            min = key[i];
+            minIndex = i;
         }
 
+	// Location of the vertex with the minimum weight to connect 
     return minIndex;
 }
 
+// Print function. I used chatGPT to help with this because I forgot pairs existed
+// and was completely lost on how to print in the desired format lol. 
 void print(vector<int> &parent, vector<vector<int>> &graph, int nvertices) {
     int totalWeight = 0;
     vector<pair<char, char>> edges;
 
-    // Loop through vertices and construct the MST edges
+    // Loop through vertices
     for (int i = 1; i < nvertices; i++) {
         if (parent[i] != -1) {
             // Calculate total weight
             totalWeight += graph[i][parent[i]];
 
-            // Add the edge to the vector (sorted as per alphabetical order)
+            // Adding edges 
             char from = char(parent[i] + 'A');
             char to = char(i + 'A');
+
+			//making sure vertices are ordered alphabetically 
             if (from > to) {
-                swap(from, to);  // Ensure edges are sorted alphabetically
+                swap(from, to);  
             }
             edges.push_back({from, to});
         }
     }
 
-    // Sort the edges alphabetically
+    // Sorting all edges alphabetically 
     sort(edges.begin(), edges.end());
 
-    // Print total weight of the MST
+	// Actually printing 
     cout << totalWeight << endl;
-
-    // Print each edge
     for (size_t i = 0; i < edges.size(); i++)
     {
         cout << edges[i].first << edges[i].second << endl;
     }
 }
 
-// Function to construct and print MST for a graph represented using adjacency matrix representation
+// prim is the function which actually implements prims algorithm and creates 
+// the MST. 
 void prim(vector<vector<int>> &graph, int nvertices) {
-    vector<int> parent(nvertices);  // Array to store the MST
-    vector<int> key(nvertices);     // Key values used to pick minimum weight edge
-    vector<bool> inMST(nvertices);  // To represent the set of vertices included in MST
+    vector<int> parent(nvertices);  // Parent stores what vertex each other vertex is connected to 
+    vector<int> key(nvertices);     // Key values represent the minimum edge weight 
+    vector<bool> inMST(nvertices);  // Stores true or false if vertex in MST
 
     // Initialize all keys as INFINITE and inMST[] as false
     for (int i = 0; i < nvertices; i++) {
@@ -63,21 +77,20 @@ void prim(vector<vector<int>> &graph, int nvertices) {
         inMST[i] = false;
     }
 
-    // Always include the first vertex in the MST.
-    key[0] = 0;      // Make key 0 so that this vertex is picked first
-    parent[0] = -1;  // First node is always the root of the MST
+	// Picking A as the arbitrary first vertex. Initializing its parent to null 
+    key[0] = 0;     
+    parent[0] = -1; 
 
+	
     for (int count = 0; count < nvertices - 1; count++) {
-        // Pick the minimum key vertex from the set of vertices not yet included in MST
-        int u = minKey(key, inMST, nvertices);
+        // Finding which vertex we can connect to with minimum weight 
+		int u = minKey(key, inMST, nvertices);
 
-        // Add the picked vertex to the MST set
+		// Adding next vertex to the MST 
         inMST[u] = true;
-
-        // Update key value and parent index of the adjacent vertices
+		
+		// Checking all other vertices to see if they can connect to u 
         for (int v = 0; v < nvertices; v++) {
-            // graph[u][v] is the weight of the edge u-v, key[v] is the minimum weight found so far
-            // Update key only if graph[u][v] is smaller and vertex v is not in MST yet
             if (graph[u][v] != -1 && !inMST[v] && graph[u][v] < key[v]) {
                 parent[v] = u;
                 key[v] = graph[u][v];
@@ -85,30 +98,32 @@ void prim(vector<vector<int>> &graph, int nvertices) {
         }
     }
 
-    // Print the constructed MST
+    // Print the MST
     print(parent, graph, nvertices);
 }
 
 int main() {
     int nvertices;
 	bool first = true; 
-    // Read number of vertices
+    
     while (cin >> nvertices) {
-        vector<vector<int>> graph(nvertices, vector<int>(nvertices));  // Create an nvertices * nvertices graph
+		// distance/adjacency matrix to represent the graph 
+        vector<vector<int>> graph(nvertices, vector<int>(nvertices));
 
-        // Input the distance matrix
+        // Reading in graph 
         for (int i = 0; i < nvertices; i++) {
             for (int j = 0; j < nvertices; j++) {
                 cin >> graph[i][j];
             }
-        }
-
-        // Call Prim's algorithm to find the MST and print it
+        } 
 		
+		// If this is the first loop, do not print a leading newline, otherwise do. 
 		if (!first)
 		{
 			cout<<endl; 
 		}
+
+		// Calling prims algorithm 
         prim(graph, nvertices);
 
 		first = false; 
