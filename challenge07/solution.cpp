e6dc3b97aa26d4f9dfecb0f904a0621a544309c7
zebulonmcknight <zmcknigh@vols.utk.edu>
2024-10-22 11:56:47 -0400

    challenge07 done

--- challenge07/solution.cpp
+++ challenge07/solution.cpp
@@ -1,7 +1,51 @@
-// Challenge 07: Adding List-Based Integers
+// Challenge 08: Repeated DNA sequences
+// Zebulon Mcknight 
+// 10/22/24 
+//
+#include <iostream> 
+#include <map> 
+#include <string> 
+using namespace std; 
 
 int main(int argc, char *argv[]) {
-    return 0;
+	string line; 
+
+	while (cin>>line)
+	{
+		map <string, int> m;
+
+		for (size_t i = 0; i <= line.size() - 9; i++) 
+		{
+			if (line.size() - i < 9)
+			{
+				break; 
+			} 
+
+			string chunk = line.substr(i, 9);  
+
+			if (m.find(chunk) == m.end())
+			{
+				m.insert({chunk, 1}); 
+			} 
+
+			else 
+			{
+				m[chunk]++; 
+			} 
+		}
+
+		for (auto& it: m)
+		{
+			if (it.second > 1)
+			{
+				cout<<it.first<<endl; 
+			} 
+		}
+
+		cout<<"-1"<<endl; 
+	} 
+		
+	return 0;
 }
 
 
